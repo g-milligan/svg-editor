f66cd26fb63fef6eb902ef9dd13d7a6d094f0d26
Gregg Milligan <gmilligan@human-element.com>
2014-12-08 22:25:17 -0500

    fix code editor bug

--- js/svg-editor.js
+++ js/svg-editor.js
@@ -11,7 +11,7 @@ function srcToDesign(src){
 function designToSrc(design){
 	design=replaceAll(design,'<','&lt;');
 	design=replaceAll(design,'>','&gt;');
-	//find out if a string is a open tag, close tag, or self-closing tag
+	//find out if a string is an open tag or close tag
 	var getTagInfo=function(tagStr){
 		var open_close='open'; var name='';
 		//remove starting <
@@ -51,60 +51,63 @@ function designToSrc(design){
 	};
 	//add markup to escaped html
 	var matchedHtmlTags=design.match(/&lt;\/?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^'"&gt;\s]+))?)+\s*|\s*)\/?&gt;/gi);
-	//for each matched starting and end tag
-	var newMatchedHtmlTags=[];
-	for(var t=0;t<matchedHtmlTags.length;t++){
-		//depending on tag type, add different markup around it
-		var matchedTagStr=matchedHtmlTags[t];
-		var info=getTagInfo(matchedTagStr);
-		//replace tag with temporary placeholder, surronded by <e> element markup
-		design=design.replace(matchedTagStr,'<e n="'+info.name.toLowerCase()+'" class="'+info.open_close+'"><<'+t+'>></e>');
-		//insert the <t> markup around the tag name
-		matchedTagStr=matchedTagStr.replace(info.name,'<n>'+info.name+'</n>');
-		//if this is an open tag
-		if(info.open_close=='open'){
- 			//select all of the key='val' OR key="val" attributes in this open tag
-			var matchedAttrs=matchedTagStr.match(/\S+=[\"|']([^"]*)[\"|']/gi);
-			//if there are any attributes in this open tag
-			if(matchedAttrs.length>0){
-				//for each matched attribute
-				var newMatchedAttrs=[];
-				for(var a=0;a<matchedAttrs.length;a++){
-					var matchedAttr=matchedAttrs[a];
-					//replace the attribute with a temporary placeholder
-					matchedTagStr=matchedTagStr.replace(matchedAttr,'<kv>>>'+a+'<<</kv>');
-					//surround the attribute key with markup
-					matchedAttr='<k>'+matchedAttr;
-					matchedAttr=matchedAttr.replace('=','</k>=');
-					//surround the val with markup
-					var valStr=matchedAttr.substring(matchedAttr.indexOf('=')+'='.length);
-					//if the value starts with ' or ", then strip it off
-					if(valStr.indexOf("'")==0){valStr=valStr.substring(1);}
-					else if(valStr.indexOf('"')==0){valStr=valStr.substring(1);}
-					//if the value ends with ' or ", then strip it off
-					if(valStr.lastIndexOf("'")==valStr.length-1){valStr=valStr.substring(0,valStr.length-1);}
-					else if(valStr.lastIndexOf('"')==valStr.length-1){valStr=valStr.substring(0,valStr.length-1);}
-					valStr=valStr.trim();
-					var strBeforeVal=matchedAttr.substring(0,matchedAttr.lastIndexOf(valStr));
-					var strAfterVal=matchedAttr.substring(strBeforeVal.length+valStr.length);
-					matchedAttr=strBeforeVal+'<v>'+valStr+'</v>'+strAfterVal;
-					//add the modified attribute string to the array
-					newMatchedAttrs.push(matchedAttr);
-				}
-				//for each matched attribute
-				for(var a=0;a<newMatchedAttrs.length;a++){
-					//restore the attribute
-					matchedTagStr=matchedTagStr.replace('>>'+a+'<<',newMatchedAttrs[a]);
+	//if any HTML tag markup was matched
+	if(matchedHtmlTags!=undefined){
+		//for each matched starting and end tag
+		var newMatchedHtmlTags=[];
+		for(var t=0;t<matchedHtmlTags.length;t++){
+			//depending on tag type, add different markup around it
+			var matchedTagStr=matchedHtmlTags[t];
+			var info=getTagInfo(matchedTagStr);
+			//replace tag with temporary placeholder, surronded by <e> element markup
+			design=design.replace(matchedTagStr,'<e n="'+info.name.toLowerCase()+'" class="'+info.open_close+'"><<'+t+'>></e>');
+			//insert the <t> markup around the tag name
+			matchedTagStr=matchedTagStr.replace(info.name,'<n>'+info.name+'</n>');
+			//if this is an open tag
+			if(info.open_close=='open'){
+				//select all of the key='val' OR key="val" attributes in this open tag
+				var matchedAttrs=matchedTagStr.match(/\S+=[\"|']([^"]*)[\"|']/gi);
+				//if there are any attributes in this open tag
+				if(matchedAttrs!=undefined&&matchedAttrs.length>0){
+					//for each matched attribute
+					var newMatchedAttrs=[];
+					for(var a=0;a<matchedAttrs.length;a++){
+						var matchedAttr=matchedAttrs[a];
+						//replace the attribute with a temporary placeholder
+						matchedTagStr=matchedTagStr.replace(matchedAttr,'<kv>>>'+a+'<<</kv>');
+						//surround the attribute key with markup
+						matchedAttr='<k>'+matchedAttr;
+						matchedAttr=matchedAttr.replace('=','</k>=');
+						//surround the val with markup
+						var valStr=matchedAttr.substring(matchedAttr.indexOf('=')+'='.length);
+						//if the value starts with ' or ", then strip it off
+						if(valStr.indexOf("'")==0){valStr=valStr.substring(1);}
+						else if(valStr.indexOf('"')==0){valStr=valStr.substring(1);}
+						//if the value ends with ' or ", then strip it off
+						if(valStr.lastIndexOf("'")==valStr.length-1){valStr=valStr.substring(0,valStr.length-1);}
+						else if(valStr.lastIndexOf('"')==valStr.length-1){valStr=valStr.substring(0,valStr.length-1);}
+						valStr=valStr.trim();
+						var strBeforeVal=matchedAttr.substring(0,matchedAttr.lastIndexOf(valStr));
+						var strAfterVal=matchedAttr.substring(strBeforeVal.length+valStr.length);
+						matchedAttr=strBeforeVal+'<v>'+valStr+'</v>'+strAfterVal;
+						//add the modified attribute string to the array
+						newMatchedAttrs.push(matchedAttr);
+					}
+					//for each matched attribute
+					for(var a=0;a<newMatchedAttrs.length;a++){
+						//restore the attribute
+						matchedTagStr=matchedTagStr.replace('>>'+a+'<<',newMatchedAttrs[a]);
+					}
 				}
 			}
+			//push the possibly modified tag html into a new array
+			newMatchedHtmlTags.push(matchedTagStr);
 		}
-		//push the possibly modified tag html into a new array
-		newMatchedHtmlTags.push(matchedTagStr);
-	}
-	//for each temporary placeholder
-	for(var t=0;t<newMatchedHtmlTags.length;t++){
-		//add the real tag back in
-		design=design.replace('<<'+t+'>>',newMatchedHtmlTags[t]);
+		//for each temporary placeholder
+		for(var t=0;t<newMatchedHtmlTags.length;t++){
+			//add the real tag back in
+			design=design.replace('<<'+t+'>>',newMatchedHtmlTags[t]);
+		}
 	}
 	return design;
 }
@@ -140,6 +143,7 @@ function updateCode(){
 	if(source!=currentDesignSrc){
 		//update the code
 		codeElem.html(currentDesignSrc);
+		//*** add wrappers around open and close <e> element tags
 		//*** add javascript events for editing source code
 	}
 }
